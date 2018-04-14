9523e59f495e16941c1266a22708d80830375229
shs49 <shs49@tscs-imac-3.resource.campus.njit.edu>
2018-04-13 20:44:10 -0400

    Raw Version of Binary Search.
    Making use of Interface Comparable
    Overriding Object method toString to print Object Employee

---
+++ DSA/src/binarySearcj/BinarySearch.java
@@ -0,0 +1,48 @@
+package binarySearcj;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class BinarySearch {
+
+	static Employee[] E = { new Employee(12), new Employee(21), new Employee(14), new Employee(32), new Employee(22),
+			new Employee(132), new Employee(324), new Employee(1001), new Employee(1231) };
+
+	public Employee bs(int high, int low, int IDs) {
+
+		
+
+		int mid = (int) Math.floor((high + low) / 2);
+		
+		if (E[mid].equals(IDs)||low==high) {
+			System.out.println("Found at " + mid);
+			return E[mid];
+		}
+
+		else if (E[mid].ID < IDs)
+			return bs(high, mid + 1, IDs);
+
+		else {
+			return bs(mid - 1, low, IDs);
+			// System.out.println("else final ");
+		}
+
+	}
+
+	public static void main(String args[]) {
+		
+		Arrays.sort(E);
+	Scanner sc = new Scanner(System.in);
+	int m = sc.nextInt();
+		BinarySearch b = new BinarySearch();
+		Employee e = b.bs(E.length - 1, 0, m);
+		System.out.println("");
+		System.out.print(e);
+		sc.close();
+		
+		
+
+	}
+
+}
---
+++ DSA/src/binarySearcj/Employee.java
@@ -0,0 +1,34 @@
+package binarySearcj;
+
+public class Employee implements Comparable<Employee>{
+
+	int ID;
+	
+public 	Employee(int x) {
+	this.ID = x;     
+}
+
+public String toString() {
+	return "Employee ID is :"+ ID;
+}
+
+public boolean equals(int x) {
+	if (this.ID == x) return true;
+	else
+	return false;
+}
+
+@Override
+public int compareTo(Employee E) {
+	if (ID == E.ID) return 0;
+	else if(ID>E.ID)  {
+		return 1;
+	}
+	else return -1;
+} 
+
+
+
+
+
+}
